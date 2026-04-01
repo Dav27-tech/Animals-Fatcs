adf210d13445cef3d3c6812007e82c6d58f1061b
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-04-01 11:25:57 +0200

    Updates js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // ==========================
 const API = {
   cat: "https://catfact.ninja/fact",
-  dog: "https://dog-api.kinduff.com/api/facts",
+  dog: "https://dogapi.dog/api/v2/facts",
 };
 
 // ==========================
@@ -53,7 +53,7 @@ async function fetchFact(type) {
     const data = await response.json();
 
     // Normalisation des réponses
-    return type === "dog" ? data.facts[0] : data.fact;
+    return type === "dog" ? data.data[0].attributes.body : data.fact;
   } catch (error) {
     throw new Error("Impossible de récupérer un fait.");
   }
@@ -69,15 +69,30 @@ typeButtons.forEach((button) => {
     typeButtons.forEach((b) => b.classList.remove("active"));
     button.classList.add("active");
     currentType = button.dataset.type;
+    factEl.textContent = "Clique pour générer un fait";
+    errorEl.textContent = "";
   });
 });
+
+// Traduction
+async function translateToFrench(text) {
+  try {
+    const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|fr`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data.responseData.translatedText;
+  } catch (error) {
+    return text; //
+  }
+}
 
 // Bouton principal
 btn.addEventListener("click", async () => {
   showLoader();
-
   try {
-    const fact = await fetchFact(currentType);
+    let fact = await fetchFact(currentType);
+    fact = await translateToFrench(fact);
     displayFact(fact);
   } catch (error) {
     displayError(error.message);
@@ -85,3 +100,5 @@ btn.addEventListener("click", async () => {
     hideLoader();
   }
 });
+
+console.log("TYPE:", currentType);
